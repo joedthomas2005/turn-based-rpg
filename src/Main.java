cdb4d4f5eaf9533d9c8243b3f8a9a2a3464a9202
joe <joedt100@gmail.com>
2022-03-22 21:45:34 +0000

    smaller main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 import java.io.IOException;
 
-import javax.crypto.AEADBadTagException;
-
-import engine.SpriteSheetParser;
-import matrices.Matrix;
-import matrices.Vector;
-
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
